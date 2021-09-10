a7f24847e5071191d2aeb1ae764210495a8f831c
Arttu Mäkelä <arttumakela@outlook.com>
2021-09-10 18:18:46 +0300

    removed useless imports

--- discordbot.py
+++ discordbot.py
@@ -1,44 +1,41 @@
-import discord
 import os
-import time
 import _thread
 import json
-from websocket import create_connection
-from dotenv import load_dotenv
+from discord.ext import commands
 import websocket
 
-
-#client = discord.Client()
-load_dotenv()
-cobalt_alert = {
-	"service":"event",
-	"action":"subscribe",
-	"worlds":"13",
-	"eventNames":"MetagameEvent"
-}
-
-alerts_cobalt = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["MetagameEvent"]}'
-logins = '{"service":"event","action":"subscribe","worlds":["1"],"eventNames":["PlayerLogin","PlayerLogout"]}'
 TOKEN = os.getenv('DISCORD_TOKEN')
-#Websocket callback functions
+channel = 795640167229554732
+alerts_cobalt = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["MetagameEvent"]}'
+logins = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["PlayerLogin","PlayerLogout"]}'
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+	print('Online')
+	await bot.get_channel(channel).send("hello")
 
 def ws_message(ws, message):
 	print(f"Websocket: {message}")
 	to_dict = json.loads(message)
+
 	if "payload" in to_dict.keys():
-		print("this is a payload")
+		print("bot just announced")
 
 def ws_open(ws):
-	ws.send(alerts_cobalt)
+	print("Opening...")
+	ws.send(logins)
+	
+def on_error(ws, error):
+	print(error)
 
-def ws_thread(*args):
+def ws_thread():
 	ws = websocket.WebSocketApp('wss://push.planetside2.com/streaming?environment=ps2&service-id=s:alerts', on_open=ws_open, on_message=ws_message)
 	ws.run_forever()
 
 _thread.start_new_thread(ws_thread, ())
+
 count = 1
 print("discorbot.py is now running")
-while True:
-	time.sleep(60)
-	print(f"Main thread: online for {count} minutes")
-	count += 1
+bot.run(TOKEN)
