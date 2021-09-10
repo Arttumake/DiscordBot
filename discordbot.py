361708d9225776a0452cb3fb39d5fc87d1de10cd
Arttu Mäkelä <arttumakela@outlook.com>
2021-09-10 20:25:21 +0300

    Added webhook to send msg to channel

--- discordbot.py
+++ discordbot.py
@@ -1,15 +1,20 @@
 import os
 import _thread
 import json
+import requests
+from discord import Webhook, RequestsWebhookAdapter
 from discord.ext import commands
 import websocket
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+WEBHOOK_ID = os.getenv('PS2_WEBHOOKID')
+WEBHOOK_TOKEN = os.getenv('PS2_WEBHOOK_TOKEN')
 channel = 795640167229554732
 alerts_cobalt = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["MetagameEvent"]}'
 logins = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["PlayerLogin","PlayerLogout"]}'
 
 bot = commands.Bot(command_prefix='!')
+webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter())
 
 @bot.event
 async def on_ready():
@@ -17,15 +22,16 @@ async def on_ready():
 	await bot.get_channel(channel).send("hello")
 
 def ws_message(ws, message):
-	print(f"Websocket: {message}")
 	to_dict = json.loads(message)
-
 	if "payload" in to_dict.keys():
-		print("bot just announced")
+		webhook.send("Alert", username='AlertsApp')
+		print("================================")
+		print("-------------ALERT--------------")
+		print("================================")
 
 def ws_open(ws):
 	print("Opening...")
-	ws.send(logins)
+	ws.send(alerts_cobalt)
 	
 def on_error(ws, error):
 	print(error)
