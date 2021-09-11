da66fb279e942a3dea1d057f92fead50b68dab1a
Arttu Mäkelä <arttumakela@outlook.com>
2021-09-11 12:52:14 +0300

    no longer notifications about alert ending

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,7 @@ import websocket
 TOKEN = os.getenv('DISCORD_TOKEN')
 WEBHOOK_ID = os.getenv('PS2_WEBHOOKID')
 WEBHOOK_TOKEN = os.getenv('PS2_WEBHOOK_TOKEN')
+
 channel = 795640167229554732
 alerts_cobalt = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["MetagameEvent"]}'
 logins = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["PlayerLogin","PlayerLogout"]}'
@@ -16,32 +17,24 @@ logins = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":[
 bot = commands.Bot(command_prefix='!')
 webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter())
 
-@bot.event
-async def on_ready():
-	print('Online')
-	await bot.get_channel(channel).send("hello")
-
 def ws_message(ws, message):
 	to_dict = json.loads(message)
-	if "payload" in to_dict.keys():
+	if "started" in to_dict.values():
 		webhook.send("Alert", username='AlertsApp')
 		print("================================")
 		print("-------------ALERT--------------")
 		print("================================")
+		print(message)
 
 def ws_open(ws):
 	print("Opening...")
 	ws.send(alerts_cobalt)
 	
-def on_error(ws, error):
-	print(error)
-
 def ws_thread():
 	ws = websocket.WebSocketApp('wss://push.planetside2.com/streaming?environment=ps2&service-id=s:alerts', on_open=ws_open, on_message=ws_message)
 	ws.run_forever()
 
-_thread.start_new_thread(ws_thread, ())
-
-count = 1
-print("discorbot.py is now running")
-bot.run(TOKEN)
+if __name__ == '__main__':
+	_thread.start_new_thread(ws_thread, ())
+	print("discorbot.py is now running")
+	bot.run(TOKEN)
