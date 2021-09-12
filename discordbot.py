17fb5af66aeb64cf6ab6792e5ce0c1e88a6c7721
Arttu Mäkelä <arttumakela@outlook.com>
2021-09-12 19:58:13 +0300

    now determines continent of alert

--- discordbot.py
+++ discordbot.py
@@ -6,35 +6,48 @@ from discord import Webhook, RequestsWebhookAdapter
 from discord.ext import commands
 import websocket
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN') # required to start discord bot
 WEBHOOK_ID = os.getenv('PS2_WEBHOOKID')
 WEBHOOK_TOKEN = os.getenv('PS2_WEBHOOK_TOKEN')
 
 channel = 795640167229554732
 alerts_cobalt = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["MetagameEvent"]}'
-logins = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["PlayerLogin","PlayerLogout"]}'
+continent_ids = {2: "Indar", 4: "Hossin", 6: "Amerish", 8: "Esamir"}
 
 bot = commands.Bot(command_prefix='!')
 webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter())
 
-def ws_message(ws, message):
+# websocket callback 
+def on_message(ws, message):
 	to_dict = json.loads(message)
-	if "payload" in to_dict.keys() and "ended" not in to_dict["payload"].values():
-		webhook.send("Alert", username='AlertsApp')
+	if "payload" not in to_dict.keys() or "heartbeat" in to_dict.values():
+		return
+
+	sub_dict = to_dict["payload"]
+	id = int(sub_dict["zone_id"])
+	continent = continent_ids[id] # a string provided that zone_id matches key in continent_id
+	
+	if sub_dict["metagame_event_state_name"] == "ended":
+		print(f"Alert ended on {continent}")
+		return
+	elif sub_dict["metagame_event_state_name"] == "started":
+		webhook.send(f"{continent} alert started", username='AlertsApp')
 		print("================================")
 		print("-------------ALERT--------------")
 		print("================================")
 		print(message)
-
-def ws_open(ws):
-	print("Opening...")
-	ws.send(alerts_cobalt)
+	else:
+		print("Something went wrong")
 	
-def ws_thread():
-	ws = websocket.WebSocketApp('wss://push.planetside2.com/streaming?environment=ps2&service-id=s:alerts', on_open=ws_open, on_message=ws_message)
+def on_open(ws):
+	print("Opening...")
+	ws.send(alerts_cobalt) # subscribe to event stream
+	
+def websocket_thread():
+	ws = websocket.WebSocketApp('wss://push.planetside2.com/streaming?environment=ps2&service-id=s:alerts', on_open=on_open, on_message=on_message)
 	ws.run_forever()
 
 if __name__ == '__main__':
-	_thread.start_new_thread(ws_thread, ())
+	_thread.start_new_thread(websocket_thread, ())
 	print("discorbot.py is now running")
 	bot.run(TOKEN)
