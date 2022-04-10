f46e6e467f98afe45a03bc8f57d313cd9161243f
Arttu Mäkelä <arttumakela@outlook.com>
2022-04-10 10:37:59 +0300

    added Oshur and an env variable

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,18 @@
 import os
 import _thread
 import json
-import requests
 from discord import Webhook, RequestsWebhookAdapter
 from discord.ext import commands
 import websocket
-import time
 
 TOKEN = os.getenv("DISCORD_TOKEN")  # required to start discord bot
 WEBHOOK_ID = os.getenv("PS2_WEBHOOKID")
 WEBHOOK_TOKEN = os.getenv("PS2_WEBHOOK_TOKEN")
+PS2_SERVICE_ID = os.getenv("PS2_SERVICE_ID")
 
 channel = 795640167229554732
 alerts_cobalt = '{"service":"event","action":"subscribe","worlds":["13"],"eventNames":["MetagameEvent"]}'
-continent_ids = {2: "Indar", 4: "Hossin", 6: "Amerish", 8: "Esamir"}
+continent_ids = {2: "Indar", 4: "Hossin", 6: "Amerish", 8: "Esamir", 10: "Oshur"}
 
 bot = commands.Bot(command_prefix="!")
 webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter())
@@ -27,7 +26,7 @@ def on_message(ws, message):
     sub_dict = payload_dict["payload"]
     id = int(sub_dict["zone_id"])
     continent = continent_ids[id]
-
+    # check for event type
     if sub_dict["metagame_event_state_name"] == "ended":
         print(f"Alert ended on {continent}")
         return
@@ -45,17 +44,21 @@ def on_open(ws):
     print("Opening...")
     ws.send(alerts_cobalt)  # subscribe to event stream
 
+
 def on_error(ws, error):
     print(error)
+
 
 def websocket_thread():
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(
-        "wss://push.planetside2.com/streaming?environment=ps2&service-id=s:cobaltalerts",
+        "wss://push.planetside2.com/streaming?environment=ps2&service-id=s:{PS2_SERVICE_ID}",
         on_open=on_open,
         on_message=on_message,
-        on_error=on_error)
+        on_error=on_error,
+    )
     ws.run_forever()
+
 
 if __name__ == "__main__":
     _thread.start_new_thread(websocket_thread, ())
